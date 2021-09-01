ab69b372fad9e037c03da71f6dafe21c38652de2
xavikortes <javiercortesveliz@gmail.com>
2021-09-01 16:26:57 +0200

    fix(rename). sprite-editor app to easytools

--- apps/easytools-e2e/src/integration/app.spec.ts
+++ apps/easytools-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('sprite-editor', () => {
+describe('easytools', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('sprite-editor', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to sprite-editor!');
+    getGreeting().contains('Welcome to easytools!');
   });
 });
--- apps/easytools/jest.config.js
+++ apps/easytools/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'sprite-editor',
+  displayName: 'easytools',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/sprite-editor',
+  coverageDirectory: '../../coverage/apps/easytools',
 };
